a10ecf54b32859e42ebbf45357a968fb10fc4eb6
Randy <randy26smc@gmail.com>
2025-02-06 17:18:55 -0600

    funcion para determinar si dos palabras son anagramas

---
+++ Anagrama.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Anagrama{
+
+
+    public static boolean esAnagrama(String palabra1, String palabra2){
+
+        //primero validamos que las palabras tengan la misma longitud
+        palabra1 = palabra1.replaceAll("\\s", "").toLowerCase();
+        palabra2 = palabra2.replaceAll("\\s", "").toLowerCase();
+
+        //ahora miramos si la longitud de palabra 1 es igual a la longitud de palabra 2
+        boolean resultado = true;
+
+        if(palabra1.length() != palabra2.length()){
+            resultado = false;
+        } 
+        else{
+
+            char[] palabra1Char = palabra1.toLowerCase().toCharArray();  
+            char[] palabra2Char = palabra2.toLowerCase().toCharArray();  
+            Arrays.sort(palabra1Char);  
+            Arrays.sort(palabra2Char);  
+            resultado = Arrays.equals(palabra1Char, palabra2Char); 
+        }
+
+
+    return resultado;
+    }
+
+
+
+
+    public static void main(String[]args){
+        //System.out.println(esAnagrama("espada", "pesada"));
+        //System.out.println(esAnagrama("hola", null));
+        Scanner sc = new Scanner(System.in);
+        String p1 = "";
+        String p2 = "";
+        System.out.println("Ingrese la primer palabra");
+        p1 = sc.nextLine();
+        System.out.println("Ingrese la segunda palabra");
+        p2 = sc.nextLine();
+
+        System.out.println(esAnagrama(p1, p2));
+    
+
+    }
+}
+
